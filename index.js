904902c9fbb84e8b9b41de2870d27e80e2569cf6
Tom Everson <tomshan225@gmail.com>
2021-11-21 09:40:39 +0000

    Update Embeds

--- index.js
+++ index.js
@@ -39,22 +39,38 @@ client.on('message', msg => {
   }
 
   if (msg.content === "?cupid") {
-    const exampleEmbed = new Discord.MessageEmbed()
-	.setColor('#90EE90')
-	.setTitle('Cupid Bot')
-	.setURL('https://discord.gg/mCEwP64faj')
-	.setAuthor('Cupid Bot #6769', 'https://cdn.discordapp.com/avatars/906824579052343326/0bb99518309ebcd8589fb3ccdd55b761.png?size=4096')
-	.setDescription('Cupid Bot is a bot that let you find your love, have relationship and so on. This bot is already open-source on github. And if you want to make a bot like this or copy some code, **!!Pls Give Me Star⭐ And Contributions.** ( or I will report to discord developer team ) ')
-	.addFields(
-    { name: '\u200B', value: '\u200B' },
-    { name: 'Commands List', value: '?help' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Github Link', value: 'https://github.com/Tom225-tech/Cupid-Bot-JS' }
-	)
+    const info = new Discord.MessageEmbed()
+      .setColor('#90EE90')
+      .setTitle('Cupid Bot')
+      .setURL('https://discord.gg/mCEwP64faj')
+      .setAuthor('Cupid Bot #6769', 'https://cdn.discordapp.com/avatars/906824579052343326/0bb99518309ebcd8589fb3ccdd55b761.png?size=4096')
+      .setDescription('Cupid Bot is a bot that let you find your love, have relationship and so on. This bot is already open-source on github. And if you want to make a bot like this or copy some code, **!!Pls Give Me Star⭐ And Contributions.** ( or I will report to discord developer team ) ')
+      .addFields(
+        { name: '\u200B', value: '\u200B' },
+        { name: 'Commands List', value: '?help' },
+        { name: '\u200B', value: '\u200B' },
+        { name: 'Github Link', value: 'https://github.com/Tom225-tech/Cupid-Bot-JS' }
+      )
 
-	.setImage('https://cdn.discordapp.com/avatars/906824579052343326/0bb99518309ebcd8589fb3ccdd55b761.png?size=4096');
-  msg.reply(exampleEmbed)
+      .setImage('https://cdn.discordapp.com/avatars/906824579052343326/0bb99518309ebcd8589fb3ccdd55b761.png?size=4096');
+    msg.reply(info)
   }
+
+  if (msg.content === "?help") {
+    const help = new Discord.MessageEmbed()
+      .setColor('#90EE90')
+      .setTitle('Commands List')
+      .setURL('https://discord.gg/mCEwP64faj')
+      .addFields(
+        { name: '**❤️Love Commands**', value: '**?love** - To Find Your Love.\n**?rateme** - To Rate Yourself\n**?status** - To check your love\'s status\n**?cupid** - Check Bot\'s Informations\n**?hug @tag** - To Hug Someone'},
+        { name: '\u200B', value: '**📣Annoy Commands**\n**?annoy @tag** - Mention Someone **25x** times' },
+        { name: 'Github Link', value: 'https://github.com/Tom225-tech/Cupid-Bot-JS' }
+      )
+
+      .setImage('https://cdn.discordapp.com/avatars/906824579052343326/0bb99518309ebcd8589fb3ccdd55b761.png?size=4096');
+    msg.reply(help)
+  }
+
 });
 
 
@@ -74,6 +90,8 @@ client.on('message', message => {
     if (!targetMember) return message.reply('Pls tag a user!! ');
     if (targetMember.user.id == `706723155800096789`) return message.reply('Go Die Loser');
     // annoying start
+    message.channel.send(`<@${targetMember.user.id}>`)
+    message.channel.send(`<@${targetMember.user.id}>`)
     message.channel.send(`<@${targetMember.user.id}>`)
     message.channel.send(`<@${targetMember.user.id}>`)
     message.channel.send(`<@${targetMember.user.id}>`)
